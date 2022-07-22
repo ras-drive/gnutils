ad5f3e0b1d5b27ac20c3a7d41c2dc8181c7d48bf
Sarah <rasdrive4165@protonmail.com>
2022-07-22 17:19:54 -0500

    added basic md5sum hashing and sum checking for test byte string

--- src/md5sum/src/main.rs
+++ src/md5sum/src/main.rs
@@ -1,3 +1,11 @@
+use md5::{Md5, Digest};
+use hex_literal::hex;
+
 fn main() {
-    println!("Hello, world!");
+    let mut hasher = Md5::new();
+    hasher.update(b"hello world");
+
+    let result = hasher.finalize();
+
+    assert_eq!(result[..], hex!("5eb63bbbe01eeed093cb22bb8f5acdc3"));
 }
